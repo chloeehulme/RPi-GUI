b882a8cc550711ba0811334d27290e1a39f47b39
chloeehulme <89981993+chloeehulme@users.noreply.github.com>
2022-04-01 12:49:17 +1100

    Update BasicGUI.py

--- BasicGUI.py
+++ BasicGUI.py
@@ -1,7 +1,14 @@
+"""
+   A basic GUI that toggles 3 LED's on and off.
+   Uses:  RPi model 3, Red LED, Green LED, Blue LED
+   Author: Chloe Hulme
+"""
+
 from tkinter import *
 import tkinter.font
 from gpiozero import LED
 import RPi.GPIO
+
 RPi.GPIO.setmode(RPi.GPIO.BCM)
 
 # hardware
@@ -9,18 +16,18 @@ led_red = LED(15)
 led_green = LED(23)
 led_blue = LED(24)
 
-# GUI DEFINITIONS
+# GUI definitions
 win = Tk()
 win.title("LED Toggle")
 myFont = tkinter.font.Font(family='Helvetica', size=12, weight="bold")
+
 
 # event functions
 # Turns off red LED if already on, else turns on red LED, turns off all other LED's
 def toggle_red():
     if led_red.is_lit:
         led_red.off()
-
-ledButtonRed["text"] = "Turn LED On"
+        ledButtonRed["text"] = "Turn LED On"
     else:
         led_green.off()
         led_blue.off()
@@ -28,7 +35,7 @@ ledButtonRed["text"] = "Turn LED On"
         ledButtonRed["text"] = "Turn LED Off"
         ledButtonGreen["text"] = "Turn LED On"
         ledButtonBlue["text"] = "Turn LED On"
-    
+
 
 # Turns off green LED if already on, else turns on green LED, turns off all other LED's
 def toggle_green():
@@ -42,8 +49,8 @@ def toggle_green():
         ledButtonGreen["text"] = "Turn LED Off"
         ledButtonRed["text"] = "Turn LED On"
         ledButtonBlue["text"] = "Turn LED On"
-        
- 
+
+
 # Turns off blue LED if already on, else turns on blue LED, turns off all other LED's
 def toggle_blue():
     if led_blue.is_lit:
@@ -51,19 +58,19 @@ def toggle_blue():
         ledButtonBlue["text"] = "Turn LED On"
     else:
         led_red.off()
-        led_green.off() 
+        led_green.off()
         led_blue.on()
         ledButtonBlue["text"] = "Turn LED Off"
         ledButtonRed["text"] = "Turn LED On"
         ledButtonGreen["text"] = "Turn LED On"
-        
+
 
 # closes window
 def close():
     RPi.GPIO.cleanup()
     win.destroy()
-    
-    
+
+
 # create widgets
 ledButtonRed = Button(win, text='Turn LED On', font=myFont, command=toggle_red, bg='IndianRed1', height=1, width=24)
 ledButtonGreen = Button(win, text='Turn LED On', font=myFont, command=toggle_green, bg='PaleGreen1', height=1, width=24)
@@ -81,3 +88,4 @@ exitButton.grid(row=3, column=4)
 win.protocol("WM_DELETE_WINDOW", close)
 
 win.mainloop()
+
